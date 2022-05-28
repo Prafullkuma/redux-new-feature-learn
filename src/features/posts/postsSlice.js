33cc40873255b3438fc9c8794ed75fbf5e8d3b5e
prafull <prafull@thinkjs.club>
2022-05-29 01:30:38 +0530

    remove state.post

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 import { sub } from 'date-fns';
-// To understand  redux basix just repalce state.posts. to state.
+// To understand  redux basix just repalce state. to state.
 const initialState = [
     {
         id: '1',
@@ -36,7 +36,7 @@ const postsSlice = createSlice({
     reducers: {
         postAdded: {
             reducer(state, action) {
-                state.posts.push(action.payload)
+                state.push(action.payload)
             },
             prepare(title, content, userId) {
                 return {
@@ -59,7 +59,7 @@ const postsSlice = createSlice({
         },
         reactionAdded(state, action) {
             const { postId, reaction } = action.payload
-            const existingPost = state.posts.find(post => post.id === postId)
+            const existingPost = state.find(post => post.id === postId)
             if (existingPost) {
                 existingPost.reactions[reaction]++
             }
@@ -67,7 +67,7 @@ const postsSlice = createSlice({
     }
 })
 
-export const selectAllPosts = (state) => state.posts.posts;
+export const selectAllPosts = (state) => state.posts;
 
 export const { postAdded, reactionAdded } = postsSlice.actions
 
